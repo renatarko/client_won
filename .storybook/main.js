853d0913a94da1121f1904bee739c39065540a36
Renata Karonlina <renatakarolinarko@gmail.com>
2022-11-21 19:01:09 -0400

    setting webpack of Storybook to absolute path

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,6 +9,10 @@ module.exports = {
     "@storybook/addon-interactions",
     "@storybook/addon-knobs",
   ],
+  webpackFinal: (config) => {
+    config.resolve.modules.push(`${process.cwd()}/src`);
+    return config;
+  },
   framework: "@storybook/react",
   core: {
     builder: "@storybook/builder-webpack5",
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,6 @@
-import GlobalStyles from "../src/styles/global";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "styles/global";
+import theme from "styles/theme";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -12,9 +14,9 @@ export const parameters = {
 
 export const decorators = [
   (Story) => (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Story />
-    </>
+    </ThemeProvider>
   ),
 ];
