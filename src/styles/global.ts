ce00f89689aef77636d67880a27b9132a8947a2c
Renata Karonlina <renatakarolinarko@gmail.com>
2022-11-21 17:38:12 -0400

    add fonts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,35 +1,45 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
-	  @font-face {
-  font-family: 'Poppins';
-  font-style: normal;
-  font-weight: 300;
-  font-display: swap;
-  src: url('/fonts/poppins-v20-latin-300.eot'); /* IE9 Compat Modes */
-  src: local(''),
-       url('/fonts/poppins-v20-latin-300.woff2') format('woff2'),
- }
 
   @font-face {
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 300;
+    src: url('/fonts/poppins-v20-latin-300.eot'); /* IE9 Compat Modes */
+    src: local(''),
+       url('/fonts/poppins-v20-latin-300.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+       url('/fonts/poppins-v20-latin-300.woff2') format('woff2'), /* Super Modern Browsers */
+       url('/fonts/poppins-v20-latin-300.woff') format('woff'), /* Modern Browsers */
+       url('/fonts/poppins-v20-latin-300.ttf') format('truetype'), /* Safari, Android, iOS */
+       url('/fonts/poppins-v20-latin-300.svg#Poppins') format('svg'); /* Legacy iOS */
+}
+
+@font-face {
   font-family: 'Poppins';
   font-style: normal;
   font-weight: 400;
-  font-display: swap;
   src: url('/fonts/poppins-v20-latin-regular.eot'); /* IE9 Compat Modes */
   src: local(''),
+       url('/fonts/poppins-v20-latin-regular.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
        url('/fonts/poppins-v20-latin-regular.woff2') format('woff2'), /* Super Modern Browsers */
- }
+       url('/fonts/poppins-v20-latin-regular.woff') format('woff'), /* Modern Browsers */
+       url('/fonts/poppins-v20-latin-regular.ttf') format('truetype'), /* Safari, Android, iOS */
+       url('/fonts/poppins-v20-latin-regular.svg#Poppins') format('svg'); /* Legacy iOS */
+}
 
-  @font-face {
+@font-face {
   font-family: 'Poppins';
   font-style: normal;
   font-weight: 600;
-  font-display: swap;
   src: url('/fonts/poppins-v20-latin-600.eot'); /* IE9 Compat Modes */
   src: local(''),
+       url('/fonts/poppins-v20-latin-600.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
        url('/fonts/poppins-v20-latin-600.woff2') format('woff2'), /* Super Modern Browsers */
- }
+       url('/fonts/poppins-v20-latin-600.woff') format('woff'), /* Modern Browsers */
+       url('/fonts/poppins-v20-latin-600.ttf') format('truetype'), /* Safari, Android, iOS */
+       url('/fonts/poppins-v20-latin-600.svg#Poppins') format('svg'); /* Legacy iOS */
+}
 
   * {
     margin: 0;
