5dbbe0b2b0b3bab44a2d18e23dbdb2250b2e12d5
Renata Karonlina <renatakarolinarko@gmail.com>
2022-11-25 16:47:37 -0400

    applying test for color and size in Logo

---
+++ src/components/Logo/test.tsx
@@ -0,0 +1,34 @@
+import { screen } from "@testing-library/react";
+import { renderWithTheme } from "utils/tests/helpers";
+
+import Logo from ".";
+
+describe("<Logo/>", () => {
+  it("should render a white label by default", () => {
+    renderWithTheme(<Logo />);
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: "#fafafa",
+    });
+  });
+});
+
+it("should render a black when colors is passed", () => {
+  renderWithTheme(<Logo color="black" />);
+  expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+    color: "#030517",
+  });
+});
+
+it("should render a normal logo when size is default", () => {
+  renderWithTheme(<Logo />);
+  expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+    width: "11rem",
+  });
+});
+
+it("should render a bigger logo", () => {
+  renderWithTheme(<Logo size="large" />);
+  expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+    width: "20rem",
+  });
+});
