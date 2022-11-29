36965b9e0edf88df3d0f209a2e7ec406211d9e53
Renata Karonlina <renatakarolinarko@gmail.com>
2022-11-29 11:35:52 -0400

    setting types to jest-styled-components

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -1,5 +1,5 @@
 import { screen } from "@testing-library/react";
-import "jest-styled-components";
+
 import { renderWithTheme } from "utils/tests/helpers";
 import { AddShoppingCart } from "@styled-icons/material-outlined/AddShoppingCart";
 
