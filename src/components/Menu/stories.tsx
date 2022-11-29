d87ed3ae362d877f869527c699a6b87bb3977c96
Renata Karonlina <renatakarolinarko@gmail.com>
2022-11-28 20:42:30 -0400

    add styles to icons and add stories

---
+++ src/components/Menu/stories.tsx
@@ -0,0 +1,14 @@
+import { Story, Meta } from "@storybook/react/types-6-0";
+import Menu from ".";
+
+export default {
+  title: "Menu",
+  component: Menu,
+  parameters: {
+    backgrounds: {
+      default: "won-dark",
+    },
+  },
+} as Meta;
+
+export const Default: Story = (args) => <Menu {...args} />;
