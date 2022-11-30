4329e2df1a7d1415f429e6cfd41cb2dcc9a5f6bd
Renata Karonlina <renatakarolinarko@gmail.com>
2022-11-30 07:55:09 -0400

    test applied to check colors: primary and secondary

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -21,7 +21,7 @@ describe("<Heading/>", () => {
   it("should render a heading with a line to the left side", () => {
     renderWithTheme(<Heading lineLeft>Won Games</Heading>);
     expect(screen.getByRole("heading", { name: /won game/i })).toHaveStyle({
-      "border-left": "0.7rem solid #3CD3C1",
+      "border-left": "0.7rem solid #F231A5",
     });
   });
 
@@ -34,5 +34,52 @@ describe("<Heading/>", () => {
         modifier: "::after",
       }
     );
+  });
+
+  it("should render a heading with a small size", () => {
+    renderWithTheme(<Heading size="small">Won Games</Heading>);
+    expect(screen.getByRole("heading", { name: /won game/i })).toHaveStyle({
+      "font-size": "1.6rem",
+    });
+
+    expect(screen.getByRole("heading", { name: /won game/i })).toHaveStyleRule(
+      "width",
+      "3rem",
+      {
+        modifier: "::after",
+      }
+    );
+  });
+
+  it("should render a Heading with a secondary line color", () => {
+    renderWithTheme(
+      <Heading lineColor="secondary" lineBottom lineLeft>
+        Lorem Ipsum
+      </Heading>
+    );
+
+    const heading = screen.getByRole("heading", { name: /lorem ipsum/i });
+    expect(heading).toHaveStyle({
+      "border-left": "0.7rem solid #3CD3C1",
+    });
+    expect(heading).toHaveStyleRule("border-bottom", "0.5rem solid #3CD3C1", {
+      modifier: "::after",
+    });
+  });
+
+  it("should render a Heading with a primary line color", () => {
+    renderWithTheme(
+      <Heading lineColor="primary" lineBottom lineLeft>
+        Lorem Ipsum
+      </Heading>
+    );
+
+    const heading = screen.getByRole("heading", { name: /lorem ipsum/i });
+    expect(heading).toHaveStyle({
+      "border-left": "0.7rem solid #F231A5",
+    });
+    expect(heading).toHaveStyleRule("border-bottom", "0.5rem solid #F231A5", {
+      modifier: "::after",
+    });
   });
 });
