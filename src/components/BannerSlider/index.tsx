f3f1a6638745d653269cd487e2c0aea02d1e21d2
Renata Karonlina <renatakarolinarko@gmail.com>
2023-04-12 14:24:08 -0400

    create BannerSlider with test when breakpoint medium to be vertical

---
+++ src/components/BannerSlider/index.tsx
@@ -0,0 +1,39 @@
+import Banner, { BannerPropos } from "components/Banner";
+import Slider, { SliderSettings } from "components/Slider";
+
+import * as S from "./styles";
+
+export type BannerSliderProps = {
+  items: BannerPropos[];
+};
+
+const settings: SliderSettings = {
+  dots: true,
+  arrows: false,
+  vertical: true,
+  verticalSwiping: true,
+  infinite: false,
+  responsive: [
+    {
+      breakpoint: 1170,
+      settings: {
+        vertical: false,
+        verticalSwiping: false,
+      },
+    },
+  ],
+};
+
+const BannerSlider = ({ items }: BannerSliderProps) => {
+  return (
+    <S.Wrapper>
+      <Slider settings={settings}>
+        {items.map((item) => (
+          <Banner key={item.title} {...item} />
+        ))}
+      </Slider>
+    </S.Wrapper>
+  );
+};
+
+export default BannerSlider;
