a3ba8871a0c1c04c4251f4b0a533d9223cdded91
Renata Karonlina <renatakarolinarko@gmail.com>
2023-04-13 15:27:31 -0400

    connecting page to data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,25 @@
-import Home from "templates/Home";
+import bannersMock from "components/BannerSlider/mock";
+import gamesMock from "components/GameCardSlider/mock";
+import highlightMock from "components/Highlight/mock";
 
-export default function Index() {
-  return <Home />;
+import Home, { HomeTemplateProps } from "templates/Home";
+
+export default function Index(props: HomeTemplateProps) {
+  return <Home {...props} />;
+}
+
+export function getServerSideProps() {
+  return {
+    props: {
+      banners: bannersMock,
+      newGames: gamesMock,
+      mostpopularHighlight: highlightMock,
+      mostpopularGames: gamesMock,
+      upcommingGames: gamesMock,
+      upcommingHighlight: highlightMock,
+      upcomminMoreGames: gamesMock,
+      freeGames: gamesMock,
+      freeGamesHighlight: highlightMock,
+    },
+  };
 }
