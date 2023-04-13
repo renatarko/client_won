266b35a366a1f14df1177c02d2ef4ffee89b0885
Renata Karonlina <renatakarolinarko@gmail.com>
2023-04-13 11:40:21 -0400

    change title and description

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,14 +9,11 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <Head>
-        <title>React Avançado - Boiderplate</title>
+        <title>Won Games</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
-        <meta
-          name="description"
-          content="A simple startet to work with TypeScript, React, NextJS and Styled Components"
-        />
+        <meta name="description" content="The best Stores in the world!" />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
