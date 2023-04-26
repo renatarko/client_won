9ff44b0a190868568917a69817bedb0068d80f87
Renata Karonlina <renatakarolinarko@gmail.com>
2023-04-26 09:51:28 -0400

    adde test to check if render Logo when the id is passed

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -4,42 +4,48 @@ import { renderWithTheme } from "utils/tests/helpers";
 import Logo from ".";
 
 describe("<Logo/>", () => {
+  it("should render the logo with id passed", () => {
+    const { container } = renderWithTheme(<Logo id="myId" />);
+
+    expect(container.querySelector("#paint_linear_myId")).toBeInTheDocument();
+  });
+
   it("should render a white label by default", () => {
     renderWithTheme(<Logo />);
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       color: "#fafafa",
     });
   });
-});
 
-it("should render a black when colors is passed", () => {
-  renderWithTheme(<Logo color="black" />);
-  expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
-    color: "#030517",
+  it("should render a black when colors is passed", () => {
+    renderWithTheme(<Logo color="black" />);
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: "#030517",
+    });
   });
-});
 
-it("should render a normal logo when size is default", () => {
-  renderWithTheme(<Logo />);
-  expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
-    width: "11rem",
+  it("should render a normal logo when size is default", () => {
+    renderWithTheme(<Logo />);
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: "11rem",
+    });
   });
-});
 
-it("should render a bigger logo", () => {
-  renderWithTheme(<Logo size="large" />);
-  expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
-    width: "20rem",
+  it("should render a bigger logo", () => {
+    renderWithTheme(<Logo size="large" />);
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: "20rem",
+    });
   });
-});
 
-it("should render a bigger logo without text if hidenOnMobile", () => {
-  renderWithTheme(<Logo hideOnMobile />);
-  expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
-    "width",
-    "5.8rem",
-    {
-      media: "(max-width: 768px)",
-    }
-  );
+  it("should render a bigger logo without text if hidenOnMobile", () => {
+    renderWithTheme(<Logo hideOnMobile />);
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
+      "width",
+      "5.8rem",
+      {
+        media: "(max-width: 768px)",
+      }
+    );
+  });
 });
