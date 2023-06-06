9621e7f0cb894896624ef1bef8102e3f8b3b3486
Renata Karonlina <renatakarolinarko@gmail.com>
2023-06-06 10:36:08 -0400

    created, initialized Apollo client

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />;
 }
 
-export function getStaticProps() {
+export async function getServerSideProps() {
   return {
     props: {
       banners: bannersMock,
